627f2056ee1240c24f88619305249c0f3335c97a
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2020-09-23 15:45:59 +0100

    Configured rollDice method to change image depending on randomInt generated when button pressed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +21,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6) + 1
-        result_text.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
